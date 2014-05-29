ffc2f20c9d47a6071b8e41fe925ea2e8f9d1048f
kurama784 <kolabrod@gmail.com>
2014-05-29 19:07:53 +0000

    App Settings
    Add comments to settings

--- app.py
+++ app.py
@@ -5,14 +5,19 @@ from views import site_index
 from views import app
 from flask.ext.sqlalchemy import SQLAlchemy
 
+# Debug Settings
 file_handler = FileHandler("debug.log","a")                                                                                             
 file_handler.setLevel(logging.WARNING)
-
 app.logger.addHandler(file_handler)
-app.register_blueprint(site_index)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
-db = SQLAlchemy(app)
 
+# Blueprints
+app.register_blueprint(site_index)
+
+# Databases
+#app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
+#db = SQLAlchemy(app)
+
+# Static Files
 if app.config['DEBUG']:
     from werkzeug import SharedDataMiddleware
     import os
